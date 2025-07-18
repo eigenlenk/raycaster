c326e3c76b60323abf0871d36221ad4b4b935f4a
Eigen Lenk <eigen.lenk@gmail.com>
2025-07-18 13:50:30 +0300

    Refactor renderer functions
    Clean up shared data and function signatures, extract full and partial wall drawing code to separate functions

--- src/include/texture.h
+++ src/include/texture.h
@@ -9,16 +9,23 @@ typedef int32_t texture_ref;
 /* Some value for your type to identify a no-texture */
 #define TEXTURE_NONE -1
 
-typedef void (*texture_coordinates_func)(float, float, int, int, int32_t*, int32_t*);
+/*
+ * Texture sampler accepting world space texture coordinates
+ * and expects texture to repeat.
+ */
+extern void (*texture_sampler_scaled)(texture_ref, float, float, uint8_t, uint8_t*, uint8_t*);
 
-extern void (*texture_sampler)(texture_ref, float, float, texture_coordinates_func, uint8_t, uint8_t*, uint8_t*);
+/*
+ * Texture sampler accepting normalized texture coordinates
+ * that should be clamped at the edges.
+ */
+extern void (*texture_sampler_normalized)(texture_ref, float, float, uint8_t, uint8_t*, uint8_t*);
 
 M_INLINED void
-debug_texture_sampler(
+debug_texture_sampler_scaled(
   texture_ref texture,
   float fx,
   float fy,
-  texture_coordinates_func coords,
   uint8_t mip_level,
   uint8_t *pixel,
   uint8_t *mask
@@ -40,17 +47,28 @@ debug_texture_sampler(
 }
 
 M_INLINED void
-texture_coordinates_scaled(float fx, float fy, int w, int h, int32_t *x, int32_t *y)
-{
-  *x = (int32_t)floorf(fx) & (w-1); // / mip_level) * mip_level;
-  *y = (int32_t)floorf(fy) & (h-1); // / mip_level) * mip_level;
-}
+debug_texture_sampler_normalized(
+  texture_ref texture,
+  float fx,
+  float fy,
+  uint8_t mip_level,
+  uint8_t *pixel,
+  uint8_t *mask
+) {
+  M_UNUSED(mip_level);
 
-M_INLINED void
-texture_coordinates_normalized(float fx, float fy, int w, int h, int32_t *x, int32_t *y)
-{
-  *x = (int32_t)(fx * (w-1)); // / mip_level) * mip_level;
-  *y = (int32_t)(fy * (h-1)); // / mip_level) * mip_level;
+  if (pixel) {
+    pixel[0] = (int32_t)(fx * 127);
+    pixel[1] = (int32_t)(fy * 127);
+    pixel[2] = (int32_t)(fy * 127);
+  }
+
+  /*
+   * Only masked textures are supported for now, so any pixel
+   * with a non-zero mask value will be drawn.
+   */
+  if (mask)
+    *mask = 255;
 }
 
 /* For passing wall texture list to map builder as part of a polygon */
